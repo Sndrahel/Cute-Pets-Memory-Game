ef933777e08307cffd3232552b1777cfb55bb460
Sndrahel <sandra.helander@hotmail.com>
2021-07-04 15:40:25 +0000

    Add main functions to js

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+function showInstructions() {
+
+}
+
+function closeInstructions() {
+
+}
+
+function shuffle(arr) {
+
+}
+
+function flipCard() {
+
+}
+
+function checkCardMatch() {
+
+}
+
+function matchPair() {
+
+}
+
+function noMatch() {
+
+}
+
+function movesCounter() {
+
+}
+
+function timer() {
+
+}
+
+// Create a function that stops the timer once all 16 cards are matched.
+function finishTime() {
+
+}
+
+function resetGame() {
+
+}
+
+function winGame() {
+
+}
+
+function winModal() {
+
+}
