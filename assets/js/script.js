5e05626cc72243e73c1556797bc35c0e8fc5bc24
Sndrahel <sandra.helander@hotmail.com>
2021-07-05 19:53:59 +0000

    Add and fix resetBoard and cardmatch functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,54 @@
 const cards = document.querySelectorAll('.card');
 
     let flippedCard = false; //checks if card has been clicked
-    let lockboard = false;
+    let lockBoard = false;
     let firstCard, secondCard;
+
+// events
+cards.forEach(card => card.addEventListener('click', flipCard));
+shuffle();
 
 
 function showInstructions() {
-
+    instructions.style.display = "block";
 }
 
 function closeInstructions() {
-
+    instructions.style.display = "none";
 }
 
 function shuffle(arr) {
 
 }
 
+(function shuffle() {
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 12);
+      card.style.order = randomPos;
+    });
+  })();
+
 function flipCard() {
-    if (lockboard) return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
-    if(!flippedCard) {
-        // first click
+    if(!flippedCard) {  // first card click
         flippedCard = true;
         firstCard = this;
 
         return;
     }
-
         // second click
-        flippedCard = false;
-        secondCard = this;
-         
-        checkCardMatch();
+        secondCard = this; 
+        checkIfCardMatch();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function checkIfCardMatch() {
 
-function checkCardMatch() {
-
-    let isMatch = firstCard.dataset.flipper === 
-    secondCard.dataset.flipper;
-
+    let isMatch = firstCard.dataset.flipper === secondCard.dataset.flipper;
+    
     isMatch ? matchPair() : noMatch();
 }
 
@@ -50,17 +56,26 @@ function matchPair() {
 
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();  
 }
 
+// board are locked until cards flip back - if no match
 function noMatch() {
-    lockboard = true;
+    lockBoard = true;
 
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-        lockboard = false;
+        resetBoard();
       }, 800);
+}
+
+//cards are reset after each round
+function resetBoard() {
+    [flippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 function movesCounter() {
@@ -75,8 +90,8 @@ function timer() {
 function finishTime() {
 
 }
-
-function resetGame() {
+ 
+function reset() {
 
 }
 
@@ -87,3 +102,21 @@ function winGame() {
 function winModal() {
 
 }
+
+/* ---- Audio Buttons ---- */
+
+audio.addEventListener('click', () => {    
+    soundButton.classList.toggle('show');
+    muteButton.classList.toggle('show');
+    if (soundOn) {
+        for (let i = 0; i < cardSounds.length; i++) {
+            cardSounds[i].muted = true;            
+        }; 
+        soundOn = false; 
+    } else {
+        for (let i = 0; i < cardSounds.length; i++) {
+            cardSounds[i].muted = false;            
+        }; 
+        soundOn = true;
+    }     
+})
