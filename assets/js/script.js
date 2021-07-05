e315a59a3775d0e6d7d1c679a303465fc51286ee
Sndrahel <sandra.helander@hotmail.com>
2021-07-05 12:56:48 +0000

    Add matchPair and noMatch function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const cards = document.querySelectorAll('.card');
 
     let flippedCard = false; //checks if card has been clicked
+    let lockboard = false;
     let firstCard, secondCard;
 
-cards.forEach(card => card.addEventListener('click', flipCard));
 
 function showInstructions() {
 
@@ -18,31 +18,49 @@ function shuffle(arr) {
 }
 
 function flipCard() {
+    if (lockboard) return;
     this.classList.add('flip');
 
-        if(!flippedCard) {
-            // first click
-            flippedCard = true;
-            firstCard = this;
-        } else {
-            // second click
-            flippedCard = false;
-            secondCard = this;
+    if(!flippedCard) {
+        // first click
+        flippedCard = true;
+        firstCard = this;
 
-            //do cards match?
-        }
+        return;
+    }
+
+        // second click
+        flippedCard = false;
+        secondCard = this;
+         
+        checkCardMatch();
 }
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 function checkCardMatch() {
 
+    let isMatch = firstCard.dataset.flipper === 
+    secondCard.dataset.flipper;
+
+    isMatch ? matchPair() : noMatch();
 }
 
 function matchPair() {
 
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 }
 
 function noMatch() {
+    lockboard = true;
 
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockboard = false;
+      }, 800);
 }
 
 function movesCounter() {
